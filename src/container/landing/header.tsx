14866df5bbfa7e129a9583378a3333e70dc02237
rezaderakhshan <rezaderakhshanich@gmail.com>
2024-12-06 01:02:15 +0330

    changing name from image container to image header container

--- src/container/landing/header.tsx
+++ src/container/landing/header.tsx
@@ -1,6 +1,6 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
-import ImageContainer from "./image-container";
+import ImageHeaderContainer from "./image-header-container";
 import ContactUs from "../../components/contact-us";
 const Header = () => {
   return (
@@ -11,28 +11,33 @@ const Header = () => {
         pt: 8,
       }}
     >
-      <Grid2 container sx={{ alignItems: "center", justifyContent: "center" }}>
-        <Grid2 xs={12} md={6}>
-          <ImageContainer />
+      <Container maxWidth={"lg"}>
+        <Grid2
+          container
+          sx={{ alignItems: "center", justifyContent: "center" }}
+        >
+          <Grid2 xs={12} md={6}>
+            <ImageHeaderContainer />
+          </Grid2>
+          <Grid2 xs={12} md={6}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "20px",
+                pl: 6,
+              }}
+            >
+              <Typography variant="h1">رضا درخشان</Typography>
+              <Typography variant="h3">طراح و توسعه دهنده وب</Typography>
+              <Typography variant="subtitle2">
+                در این قسمت توضیح مختصری درباره خود بنویسید
+              </Typography>
+              <ContactUs />
+            </Box>
+          </Grid2>
         </Grid2>
-        <Grid2 xs={12} md={6}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "20px",
-              pl: 6,
-            }}
-          >
-            <Typography variant="h1">رضا درخشان</Typography>
-            <Typography variant="h3">طراح و توسعه دهنده وب</Typography>
-            <Typography variant="subtitle2">
-              در این قسمت توضیح مختصری درباره خود بنویسید
-            </Typography>
-            <ContactUs />
-          </Box>
-        </Grid2>
-      </Grid2>
+      </Container>
     </Box>
   );
 };
---
+++ src/container/landing/image-header-container.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Box } from "@mui/material";
+import { headerImg } from "../../../public/images";
+import Image from "next/image";
+
+const ImageHeaderContainer = () => {
+  return (
+    <Box
+      sx={{
+        "& img": {
+          borderRadius: "50%",
+          ml: "auto",
+        },
+        display: "flex",
+        justifyContent: "flex-end",
+      }}
+    >
+      <Image
+        src={headerImg}
+        alt="Photo of Reza Derakhshan"
+        placeholder="blur"
+        width={600}
+        height={600}
+      />
+    </Box>
+  );
+};
+
+export default ImageHeaderContainer;
